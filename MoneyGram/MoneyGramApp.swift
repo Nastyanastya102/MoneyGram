d0d366a4525510e1b8dddfa1d2a3a6713ccda971
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-21 21:30:38 -0700

    Initial Commit

---
+++ MoneyGram/MoneyGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyGramApp.swift
+//  MoneyGram
+//
+//  Created by Nastya Klyashtorna on 2024-10-21.
+//
+
+import SwiftUI
+
+@main
+struct MoneyGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
