e1f2d6e2127c1baba70e14d0e1c61e72fdf96784
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-23 22:06:31 -0700

    Chnage monye gram app to weather app

--- MoneyGram/ContentView.swift
+++ MoneyGram/ContentView.swift
@@ -11,42 +11,32 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Image("money-bg")
-                    .resizable()
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-                
+                LinearGradientView(top: .blue, bottom: Color("LightBlue"))
                 VStack {
-                    Spacer()
-                    Text("Welcome to your Expenses Tracker")
-                        .font(.title)
-                        .foregroundColor(Color("DeepBlue"))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 12)
-                        .dynamicTypeSize(.xSmall)
-                    
-                    Spacer()
-                    VStack {
-                        NavigationLink(destination: ExpensesView()) {
-                            HStack {
-                                Image(systemName: "dollarsign")
-                                    .imageScale(.medium)
-                                    .foregroundStyle(Color.gray)
-                                    .padding(.leading, 4)
-                                
-                                Text("Start tracking your expenses!")
-                                    .padding(10)
-                            }
-                            .foregroundColor(Color.gray)
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                        }
+                    CityTextView(cityName: "Cupertino, CA")
+                    VStack(spacing: 10) {
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 180, height: 180)
+                        Text("78°")
+                            .font(.system(size: 70, weight: .medium))
+                            .foregroundColor(.white)
                     }
-                    .padding(10)
+                    .padding(.bottom, 42)
+                    
+                    HStack(spacing: 24) {
+                        SmallWeatherView(day: "Mon", icon: "cloud.sun.fill", temperature: 78)
+                        SmallWeatherView(day: "Tue", icon: "sun.max.fill", temperature: 82)
+                        SmallWeatherView(day: "Wed", icon: "sun.max.fill", temperature: 80)
+                        SmallWeatherView(day: "Thu", icon: "cloud.sun.rain.fill", temperature: 57)
+                        SmallWeatherView(day: "Fri", icon: "cloud.sun.rain.fill", temperature: 65)
+                    }
+                    Spacer()
+                    ButtonView()
+                    Spacer()
                 }
-                .padding()
-                .frame(maxHeight: .infinity, alignment: .bottom)
             }
         }
     }
@@ -54,4 +44,64 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct SmallWeatherView: View {
+    var day: String = "Mon"
+    var icon: String = "cloud.sun.fill"
+    var temperature: Int = 78
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .foregroundColor(.white)
+                .font(.system(size: 24))
+            Image(systemName: icon)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 24, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct LinearGradientView: View {
+    var top: Color
+    var bottom: Color
+    
+    var body: some View {
+        LinearGradient(colors: [top, bottom],
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View {
+        Text(cityName)
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct ButtonView: View {
+    var body: some View {
+        Button {
+            print("tapped")
+        } label: {
+            Text("Change day time")
+                .font(.system(size: 20, weight: .medium))
+                .foregroundColor(.blue)
+                .frame(width: 280, height: 70)
+                .background(Color.white)
+                .cornerRadius(14)
+        }
+    }
 }
